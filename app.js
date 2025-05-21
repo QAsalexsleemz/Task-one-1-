6580be16066327d27ee2daa7cae5a4f77d42ab0e
QAsalexsleemz <salim.adeshina@gmail.com>
2025-05-21 03:38:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const firstDayoftheweek = "Monday"
+const secondDayoftheweek = "Tuesday"
+
+
+
+
+
+
+console.log(firstDayoftheweek)
+console.log(secondDayoftheweek)
+
+console.log(typeof(firstDayoftheweek))
---
+++ task1.js
@@ -0,0 +1,19 @@
+const daysOftheweek = ("Monday; Tuesday; Wednesday; Thursday; Friday; Saturday; Sunday")
+
+const currentDayoftheweek = "Wednesday"
+
+const position = 3
+
+const output = "Today is " + currentDayoftheweek + "." + " It is the " + position + "rd" + " day of the week."
+
+console.log(daysOftheweek)
+
+console.log(currentDayoftheweek)
+
+console.log(typeof(currentDayoftheweek))
+
+console.log(position)
+
+console.log(typeof(position))
+
+console.log(output)
