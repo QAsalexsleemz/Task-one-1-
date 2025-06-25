032d3c43dd564a1b438f5d254e3a20abdd6e5d35
QAsalexsleemz <salim.adeshina@gmail.com>
2025-06-25 08:38:47 -0700

    Arrayassnmt

---
+++ Arrayassnmt.js
@@ -0,0 +1,11 @@
+const friends = ['Sanya', 'Wale', 'Oloye', 'Dogho', 'Ibrahim']
+
+console.log(friends)
+
+const friendS = new Array('Sanya', 'Wale', 'Oloye', 'Dogho', 'Ibrahim')
+
+console.log(friendS)
+
+const bestFriend = [friendS[3]]
+
+console.log(bestFriend)
--- test.js
+++ test.js
@@ -1,23 +1,33 @@
-// let output = 10 < 20
-// console.log(output)
-// let davidAge = 10
-// let samuelAge = 15
-// let age = (davidAge < samuelAge)
-// console.log(age)
-// let input = 4 == 3
-// console.log(input)
-// let number = 4 == 4
-// console.log(number)
-// let temiAge = 25
-// let adeshinaAge = 41
-// let age = (temiAge == adeshinaAge)
-// console.log(age)
+let output = 10 < 20
 
-// let numbers = 4 === 4
-// console.log(numbers)
+console.log(output)
 
-// let integer = 4 === "4"
-// console.log(integer)
+let davidAge = 10
+let samuelAge = 15
+let age = (davidAge < samuelAge)
+
+console.log(age)
+let input = 4 == 3
+
+console.log(input)
+let number = 4 == 4
+
+console.log(number)
+
+let temiAge = 25
+let adeshinaAge = 41
+let agE = (temiAge == adeshinaAge)
+
+console.log(age)
+
+let numbers = 4 === 4
+
+console.log(numbers)
+
+let integer = 4 === "4"
+
+console.log(integer)
 
 let integers = 4 == "4"
+
 console.log(integers)
